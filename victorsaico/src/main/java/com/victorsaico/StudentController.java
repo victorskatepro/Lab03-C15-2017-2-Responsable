babacec5f94237a19d5ab187cfd0e0f7fd8f671d
alejandro <victor.saico@tecsup.edu.pe>
2017-08-29 09:25:24 -0500

    First Message

---
+++ victorsaico/src/main/java/com/victorsaico/StudentController.java
@@ -0,0 +1,13 @@
+package com.victorsaico;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudentController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
