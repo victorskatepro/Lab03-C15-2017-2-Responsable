8979830f06979589a17f03b9ee31249aa51eb8f0
Rodrigo <exit>
2017-08-29 09:53:51 -0500

    Nuevos cambios, Victor

--- victorsaico/src/main/java/com/victorsaico/StudentController.java
+++ victorsaico/src/main/java/com/victorsaico/StudentController.java
@@ -9,5 +9,9 @@ public class StudentController
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        showMessage();
+    }
+    private static void showMessage() {
+    	System.out.println("Hola saico");
     }
 }
